1d7b0200a3d52c913de9302c5d6468ad4d889e9c
4lagest <zakirodion@gmail.com>
2022-10-29 09:36:27 +0300

    HW4

---
+++ HomeWork4_Zakshevskij_UTS-22/Program.cs
@@ -0,0 +1,169 @@
+﻿using System;
+using System.Globalization;
+
+namespace hw4
+{
+    class Programm
+    {
+        public static int CorrectInt(string b)
+        {
+            
+            int a;
+            do
+            {
+                if (int.TryParse(b, out a))
+                {
+                    break;
+                }
+                Console.WriteLine("Число должно быть целым и положительным");
+                b = Console.ReadLine();
+            } while (true);
+            if (a < 0)
+            {
+                a = a * -1;
+            }
+            return a;
+        }
+        public static void InitArray(int[] X)
+        {
+
+            Random r = new Random();
+            for (int i = 0; i < X.Length; i++)
+            {
+                X[i] = r.Next(-50, 50);
+            }
+        }
+        public static int Main()
+        {
+            int f = 1;
+            do
+            {
+                Console.Write("Введите длину одномерного массива: ");
+                string R = Console.ReadLine();
+                int a = CorrectInt(R);
+                int[] X = new int[a];
+                InitArray(X);
+                Console.WriteLine("Исходный массив:");
+                var str = string.Join(" ", X);
+                Console.WriteLine(str);
+                Console.WriteLine($"Максимальный элемент данного массива: {X.Max()}");
+                int IndexMax = Array.IndexOf(X, X.Max());
+                int LastPIndex = 0;
+                Array.Reverse(X);
+                for (int i = 0; i < X.Length; i++)
+                {
+                    if (X[i] > 0)
+                    {
+                        LastPIndex = i;
+                        break;
+                    }
+                }
+                Array.Reverse(X);
+                LastPIndex = X.Length - LastPIndex - 1;
+                int SumToLast = 0;
+                for (int i = 0; i < LastPIndex; i++)
+                {
+                    SumToLast += X[i];
+                }
+                Console.WriteLine($"Сумма до последнего положительного элемента: {SumToLast}");
+                if (X.Max() > SumToLast)
+                {
+                    for (int i = 0; i < X.Length; i++)
+                    {
+                        if ((Math.Abs(X[i]) <= X.Max()) && (Math.Abs(X[i]) >= SumToLast))
+                        {
+                            X[i] = 0;
+                        }
+                    }
+                }
+                else if (X.Max() < SumToLast)
+                {
+                    for (int i = 0; i < X.Length; i++)
+                    {
+                        if ((Math.Abs(X[i]) >= X.Max()) && (Math.Abs(X[i]) <= SumToLast))
+                        {
+                            X[i] = 0;
+                        }
+                    }
+                }
+                Console.WriteLine("Сжатый массив:");
+                var str2 = string.Join(" ", X);
+                Console.WriteLine(str2);
+                Console.WriteLine("Введите размер квадратной матрицы:");
+                int A;
+                R = Console.ReadLine();
+                A = CorrectInt(R);
+                int B = A;
+                int[,] matr = new int[A,B];
+                Random ran = new Random();
+                Console.WriteLine("Исходная матрица:");
+                for (int i = 0; i < A; i++)
+                {
+                    for (int j = 0; j < B; j++)
+                    {
+                        matr[i, j] = ran.Next(-10, 50);
+                        Console.Write("{0}\t", matr[i, j]);
+                    }
+                    Console.WriteLine();
+                }
+                int count = 0;
+                int[] sum = new int[B];
+                int sum1 = 0;
+                int[] P = new int[A];
+                for (int i = 0; i < A; i++)
+                {
+                    for (int j = 0; j < B; j++)
+                    {
+                        sum1 += matr[i, j];
+                    }
+                    sum[i] = sum1;
+                    sum1 = 0;
+                }
+                var str3 = string.Join(" ", sum);
+                Console.WriteLine(str3);
+                for (int i = 0; i < A; i++)
+                {
+                    for (int j = 0; j < B; j++)
+                    {
+                        P[i] = matr[i, j];
+                    }
+
+                }
+                /*int[] P = new int[A];
+                bool h = false;
+                int l = 0;
+                int sum = 0;
+                while(true)
+                { 
+                    for (int i = 0; i < A; i++)
+                    {
+                        P[i] = matr[i, l];  
+                    }
+                    for(int i = 0; i < B; i++)
+                    {
+                        if (P[i] < 0)
+                        {
+                            h = false;
+                            break;
+                        }
+                        else
+                        {
+                            h = true;
+                        }
+                    }
+                    if (h==true)
+                    {
+                        break;
+                    }
+                    l++;
+                    sum = 0;
+                }
+                Console.WriteLine(l);
+                Console.WriteLine(sum);*/
+                Console.WriteLine("Повторить выполнение (1 - да, 0 - нет)");
+                f = Convert.ToInt32(Console.ReadLine());
+            } while (f == 1);
+            return 0;
+        }
+    }
+}
